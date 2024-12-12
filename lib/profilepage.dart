b7819fe7f6f4080843feb6a5fadd5bba59c17aa1
maulinanl <61910881+maulinanl@users.noreply.github.com>
2024-12-13 02:33:18 +0700

    Update profilepage.dart

--- lib/profilepage.dart
+++ lib/profilepage.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
 import 'package:fp_kelompok3/loginpage.dart';
 
 class ProfilePage extends StatefulWidget {
   final String username;
   final String email;
 
-  ProfilePage({required this.username, required this.email});
+  const ProfilePage({super.key, required this.username, required this.email});
 
   @override
   _ProfilePageState createState() => _ProfilePageState();
@@ -17,34 +18,107 @@ class _ProfilePageState extends State<ProfilePage> {
   final TextEditingController fullNameController = TextEditingController();
   final TextEditingController phoneNumberController = TextEditingController();
   final TextEditingController dateOfBirthController = TextEditingController();
+
   String selectedGender = 'Male';
+  String imagePath = '';
+
+  final List<String> availableImages = [
+    'Image/avatar1.jpg',
+    'Image/avatar2.jpg',
+    'Image/avatar3.jpg',
+    'Image/avatar4.jpg',
+    'Image/avatar5.jpg',
+    'Image/avatar6.jpg'
+  ];
 
   @override
   void initState() {
     super.initState();
-    fullNameController.text = widget.username; // Isi dengan username
+    fullNameController.text = widget.username;
+    _loadProfileData();
+  }
+
+  Future<void> _loadProfileData() async {
+    final uid = FirebaseAuth.instance.currentUser?.uid;
+    if (uid != null) {
+      final snapshot =
+          await FirebaseFirestore.instance.collection('users').doc(uid).get();
+      final data = snapshot.data();
+      if (data != null) {
+        setState(() {
+          imagePath = data['imagePath'] ?? '';
+          phoneNumberController.text = data['phoneNumber'] ?? '';
+          dateOfBirthController.text = data['dateOfBirth'] ?? '';
+          selectedGender = data['gender'] ?? 'Male';
+        });
+      }
+    }
   }
 
   Future<void> _saveProfile() async {
     try {
       final uid = FirebaseAuth.instance.currentUser?.uid;
 
+      // Validasi nomor telepon
+      final phoneNumber = phoneNumberController.text.trim();
+      if (!RegExp(r'^\d+$').hasMatch(phoneNumber)) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+              content: Text('Phone number must contain only numbers.')),
+        );
+        return; // Hentikan proses jika validasi gagal
+      }
+
       if (uid != null) {
         await FirebaseFirestore.instance.collection('users').doc(uid).update({
           'fullName': fullNameController.text.trim(),
-          'phoneNumber': phoneNumberController.text.trim(),
+          'phoneNumber': phoneNumber,
           'dateOfBirth': dateOfBirthController.text.trim(),
           'gender': selectedGender,
+          'imagePath': imagePath, // Menyimpan path gambar
         });
 
         ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Profile saved successfully!')),
+          const SnackBar(content: Text('Profile saved successfully!')),
         );
       }
     } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Failed to save profile: $e')),
       );
+    }
+  }
+
+  Future<void> _selectImage() async {
+    final selectedImage = await showDialog<String>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Select an Image'),
+          content: SizedBox(
+            width: double.maxFinite,
+            child: ListView.builder(
+              itemCount: availableImages.length,
+              itemBuilder: (context, index) {
+                final imagePath = availableImages[index];
+                return ListTile(
+                  leading: Image.asset(imagePath, width: 50, height: 50),
+                  title: Text('Image ${index + 1}'),
+                  onTap: () {
+                    Navigator.pop(context, imagePath); // Kembalikan path gambar
+                  },
+                );
+              },
+            ),
+          ),
+        );
+      },
+    );
+
+    if (selectedImage != null) {
+      setState(() {
+        imagePath = selectedImage;
+      });
     }
   }
 
@@ -57,120 +131,124 @@ class _ProfilePageState extends State<ProfilePage> {
   }
 
   @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    backgroundColor: Color(0xFFF5F5EB),
-    body: Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Bagian atas: Tombol back dan Profile
-          Row(
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF5F5EB),
+      resizeToAvoidBottomInset: true,
+      body: SingleChildScrollView(
+        // Tambahkan ini
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              IconButton(
-                icon: Icon(Icons.arrow_back, color: Colors.black),
-                onPressed: () => Navigator.pop(context),
+              // Bagian atas: Tombol back dan Profile
+              Row(
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.arrow_back, color: Colors.black),
+                    onPressed: () => Navigator.pop(context),
+                  ),
+                  const SizedBox(width: 10),
+                  const Text(
+                    'Profile',
+                    style: TextStyle(
+                      fontFamily: 'BeVietnamPro',
+                      color: Colors.black,
+                      fontSize: 23,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
               ),
-              SizedBox(width: 10),
-              Text(
-                'Profile',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
+              const SizedBox(height: 20),
+
+              // Foto Profil
+              Center(
+                child: Stack(
+                  children: [
+                    CircleAvatar(
+                      radius: 70,
+                      backgroundColor: const Color(0xFFB3907A),
+                      child: CircleAvatar(
+                        radius: 66,
+                        backgroundImage: imagePath.isEmpty
+                            ? const AssetImage('Image/profile.jpg')
+                            : AssetImage(imagePath),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 4,
+                      child: GestureDetector(
+                        onTap: _selectImage,
+                        child: Container(
+                          padding: const EdgeInsets.all(10),
+                          decoration: const BoxDecoration(
+                            color: Color(0xFFB3907A),
+                            shape: BoxShape.circle,
+                          ),
+                          child: const Icon(
+                            Icons.edit,
+                            color: Color(0xFFF5F5EB),
+                            size: 20,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 15),
+              // Pastikan konten lain cukup ruang dengan scroll
+              _buildLabeledFormField('Full Name', fullNameController),
+              _buildLabeledFormField('Phone Number', phoneNumberController,
+                  isNumeric: true),
+              _buildDateOfBirthField(),
+              _buildGenderDropdown(),
+              const SizedBox(height: 20),
+              // Tombol Save
+              Center(
+                child: ElevatedButton(
+                  onPressed: () async {
+                    await _saveProfile();
+                    Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginPage()),
+                      (Route<dynamic> route) => false,
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFFB3907A),
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 16, horizontal: 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      side: const BorderSide(color: Colors.black),
+                    ),
+                  ),
+                  child: Text(
+                    'Save',
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
                 ),
               ),
             ],
           ),
-          SizedBox(height: 20),
-
-          // Foto Profil
-          Center(
-            child: Stack(
-              children: [
-                CircleAvatar(
-                  radius: 70,
-                  backgroundColor: Color(0xFFB3907A), // Warna bingkai
-                  child: CircleAvatar(
-                    radius: 66,
-                    backgroundImage: AssetImage('Image/profile.jpg'), // Ganti dengan URL gambar Anda
-                  ),
-                ),
-                Positioned(
-                  bottom: 0,
-                  right: 4,
-                  child: GestureDetector(
-                    onTap: () {
-                      // Logic for editing profile picture
-                    },
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: Color(0xFFB3907A),
-                        shape: BoxShape.circle,
-                      ),
-                      child: Icon(
-                        Icons.edit,
-                        color: Color(0xFFF5F5EB),
-                        size: 20,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-            SizedBox(height: 20),
-            // Form input sesuai UI Anda
-            _buildLabeledFormField('Full Name', fullNameController),
-            SizedBox(height: 15),
-            _buildLabeledFormField('Phone Number', phoneNumberController,
-                keyboardType: TextInputType.phone),
-            SizedBox(height: 15),
-            _buildDateOfBirthField(),
-            SizedBox(height: 15),
-            _buildGenderDropdown(),
-            Spacer(),
-            // Tombol Save
-            Center(
-              child: ElevatedButton(
-                onPressed: () async {
-                  await _saveProfile(); 
-                  Navigator.pushAndRemoveUntil(
-                    context,
-                    MaterialPageRoute(builder: (context) => LoginPage()), 
-                    (Route<dynamic> route) => false, 
-                  );
-                },
-                child: Text(
-                  'Save',
-                  style: TextStyle(color: Colors.white, fontSize: 18),
-                ),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFFB3907A),
-                  padding: EdgeInsets.symmetric(vertical: 16, horizontal: 50),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
-                    side: BorderSide(color: Colors.black),
-                  ),
-                ),
-              ),
-            ),
-          ],
         ),
       ),
     );
   }
 
   Widget _buildLabeledFormField(String label, TextEditingController controller,
-      {TextInputType keyboardType = TextInputType.text}) {
+      {TextInputType keyboardType = TextInputType.text,
+      bool isNumeric = false}) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
           label,
-          style: TextStyle(
+          style: const TextStyle(
             fontSize: 16,
             fontWeight: FontWeight.bold,
             color: Color(0xFFB3907A),
@@ -178,68 +256,80 @@ Widget build(BuildContext context) {
         ),
         TextField(
           controller: controller,
-          keyboardType: keyboardType,
-          decoration: InputDecoration(
+          keyboardType: isNumeric ? TextInputType.phone : keyboardType,
+          inputFormatters:
+              isNumeric ? [FilteringTextInputFormatter.digitsOnly] : [],
+          onChanged: (value) {
+            if (isNumeric && !RegExp(r'^\d*$').hasMatch(value)) {
+              controller.text = value.replaceAll(RegExp(r'\D'), '');
+              controller.selection = TextSelection.fromPosition(
+                TextPosition(offset: controller.text.length),
+              );
+            }
+          },
+          decoration: const InputDecoration(
             filled: true,
             fillColor: Color(0xFFF5F5EB),
             border: InputBorder.none,
             contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
           ),
         ),
-        Divider(color: Color(0xFFB3907A)),
+        const Divider(color: Color(0xFFB3907A)),
       ],
     );
   }
 
-Widget _buildDateOfBirthField() {
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      Text(
-        'Date of Birth',
-        style: TextStyle(
-          fontSize: 16,
-          fontWeight: FontWeight.bold,
-          color: Color(0xFFB3907A),
-        ),
-      ),
-      TextField(
-        controller: dateOfBirthController,
-        readOnly: true,
-        decoration: InputDecoration(
-          filled: true,
-          fillColor: Color(0xFFF5F5EB), // Warna yang sama
-          border: InputBorder.none,
-          contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-          suffixIcon: IconButton(
-            icon: Icon(Icons.calendar_today, color: Color(0xFFB3907A)), // Warna ikon
-            onPressed: () async {
-              DateTime? pickedDate = await showDatePicker(
-                context: context,
-                initialDate: DateTime.now(),
-                firstDate: DateTime(1900),
-                lastDate: DateTime.now(),
-              );
-              if (pickedDate != null) {
-                setState(() {
-                  dateOfBirthController.text =
-                      "${pickedDate.toLocal()}".split(' ')[0];
-                });
-              }
-            },
+  Widget _buildDateOfBirthField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          'Date of Birth',
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Color(0xFFB3907A),
           ),
         ),
-      ),
-      Divider(color: Color(0xFFB3907A)), // Garis pembatas
-    ],
-  );
-}
+        TextField(
+          controller: dateOfBirthController,
+          readOnly: true,
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: const Color(0xFFF5F5EB), // Warna yang sama
+            border: InputBorder.none,
+            contentPadding:
+                const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            suffixIcon: IconButton(
+              icon: const Icon(Icons.calendar_today,
+                  color: Color(0xFFB3907A)), // Warna ikon
+              onPressed: () async {
+                DateTime? pickedDate = await showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(1900),
+                  lastDate: DateTime.now(),
+                );
+                if (pickedDate != null) {
+                  setState(() {
+                    dateOfBirthController.text =
+                        "${pickedDate.toLocal()}".split(' ')[0];
+                  });
+                }
+              },
+            ),
+          ),
+        ),
+        const Divider(color: Color(0xFFB3907A)), // Garis pembatas
+      ],
+    );
+  }
 
   Widget _buildGenderDropdown() {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
+        const Text(
           'Gender',
           style: TextStyle(
             fontSize: 16,
@@ -257,20 +347,21 @@ Widget _buildDateOfBirthField() {
               if (value != null) selectedGender = value;
             });
           },
-          decoration: InputDecoration(
+          decoration: const InputDecoration(
             filled: true,
             fillColor: Color(0xFFF5F5EB), // Warna yang sama
             border: InputBorder.none,
             contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
           ),
-          dropdownColor: Color(0xFFF5F5EB), // Warna dropdown
-          style: TextStyle(
+          dropdownColor: const Color(0xFFF5F5EB), // Warna dropdown
+          style: const TextStyle(
             color: Colors.brown, // Warna teks dropdown
             fontSize: 14,
           ),
-          icon: Icon(Icons.arrow_drop_down, color: Color(0xFFB3907A)), // Warna ikon
+          icon: const Icon(Icons.arrow_drop_down,
+              color: Color(0xFFB3907A)), // Warna ikon
         ),
-        Divider(color: Color(0xFFB3907A)), // Garis pembatas
+        const Divider(color: Color(0xFFB3907A)), // Garis pembatas
       ],
     );
   }
